0ede9706dfe3deb7eea76a5168dea15d2a60cca2
necatiozturk <nozturk@islander.tamucc.edu>
2015-11-04 17:34:06 -0600

    Connections, Matches, Viewed Me, and Search now all have directory functionality. Added a 2nd json file to be used by Pending tab (temporarily)

--- facultyApp/app/controllers/index.js
+++ facultyApp/app/controllers/index.js
@@ -2,42 +2,49 @@ $.profileButton.addEventListener('click', function(e)
 {
 	var newWindow = Alloy.createController('profile').getView();
 	newWindow.open();
+	$.index.close();
 });
 
 $.messagesButton.addEventListener('click', function(e)
 {
 	var newWindow = Alloy.createController('messages').getView();
 	newWindow.open();
+	$.index.close();
 });
 
 $.searchButton.addEventListener('click', function(e)
 {
 	var newWindow = Alloy.createController('search').getView();
 	newWindow.open();
+	$.index.close();
 });
 
 $.matchesButton.addEventListener('click', function(e)
 {
 	var newWindow = Alloy.createController('matches').getView();
 	newWindow.open();
+	$.index.close();
 });
 
 $.connectionsButton.addEventListener('click', function(e)
 {
 	var newWindow = Alloy.createController('connections').getView();
 	newWindow.open();
+	$.index.close();
 });
 
 $.viewedMeButton.addEventListener('click', function(e)
 {
 	var newWindow = Alloy.createController('viewedMe').getView();
 	newWindow.open();
+	$.index.close();
 });
 
 $.profileBar.addEventListener('click', function(e)
 {
 	var newWindow = Alloy.createController('profile').getView();
 	newWindow.open();
+	$.index.close();
 });
 
 $.index.open();
