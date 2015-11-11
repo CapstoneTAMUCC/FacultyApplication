5c25503fc3cc82db594be80c601d52e4923e1750
necatiozturk <nozturk@islander.tamucc.edu>
2015-11-10 23:59:18 -0600

    READ AND INSERT FOR PROFILE added

--- facultyApp/app/controllers/index.js
+++ facultyApp/app/controllers/index.js
@@ -1,6 +1,6 @@
 $.profileButton.addEventListener('click', function(e)
 {
-	var newWindow = Alloy.createController('test').getView();
+	var newWindow = Alloy.createController('profile').getView();
 	newWindow.open();
 	$.index.close();
 });
--- facultyApp/app/controllers/profile.js
+++ facultyApp/app/controllers/profile.js
@@ -17,9 +17,11 @@ $.editButton1.addEventListener('click', function(e)
 		e.source.value = true;
 		e.source.title = 'DONE';
 		e.source.backgroundColor = 'black';
-		$.additionalText1.enabled = 'true';
-		$.additionalText2.enabled = 'true';
-		$.additionalText3.enabled = 'true';
+		//$.additionalText1.enabled = 'true';
+		//$.additionalText2.enabled = 'true';
+		//$.additionalText3.enabled = 'true';
+		$.nameField.enabled = 'true';
+		$.nameField.backgroundColor = 'black';
 		$.educationText.enabled = 'true';
 		$.contactInfoText.enabled = 'true';
 		$.projectText.enabled = 'true';
@@ -31,9 +33,11 @@ $.editButton1.addEventListener('click', function(e)
 		e.source.value = false;
 		e.source.title = 'EDIT';
 		e.source.backgroundColor = '#800000';
-		$.additionalText1.enabled = 'false';
-		$.additionalText2.enabled = 'false';
-		$.additionalText3.enabled = 'false';
+		//$.additionalText1.enabled = 'false';
+		//$.additionalText2.enabled = 'false';
+		//$.additionalText3.enabled = 'false';
+		$.nameField.enabled = 'false';
+		$.nameField.backgroundColor = '#800000';
 		$.educationText.enabled = 'false';
 		$.contactInfoText.enabled = 'false';
 		$.projectText.enabled = 'false';
@@ -203,4 +207,112 @@ $.question2no.addEventListener('click', function(e)
     }
 });
 
+function getCheckboxValue(e)	//will be used in insertData
+{
+	if (e.value == true)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+function readData(){
+	//function to use HTTP to connect to a web server and transfer the data. 
+	var sendit = Ti.Network.createHTTPClient({ 
+	onerror: function(e){ 	
+		Ti.API.debug(e.error); 	
+		alert('There was an error during the connection2222'); 
+	
+	}, 	
+	timeout:1000, 
+	});
+	
+	//Here you have to change it for your local ip 
+	sendit.open('GET', '52.32.54.34/php/read_user_list.php');  
+	sendit.send();
+	
+	sendit.onload = function() {
+		var json = JSON.parse(this.responseText);
+		var json = json.NAME;
+		for( var i=0; i<json.length; i++) {
+			if ( json[i].USER_ID == '1234') //used to be 1234 only without quotations
+			{
+				$.nameField.value = json[i].NAME;
+				$.educationText.value = json[i].EDUCATION;
+				$.projectText.value = json[i].CURRENT_PROJ;
+				$.expertiseText.value = json[i].AREA_EXPERTISE;
+				$.committeeText.value = json[i].COMMITTEES;
+				$.otherInterestText.value = json[i].OTHER_INTRESTS;
+				$.contactInfoText.value = json[i].O_CONTACT_INFO;
+				
+				if (json[i].EXPAND == 1)
+				{
+					$.question1yes.value = true;
+					$.question1yes.backgroundColor = '#007690';
+					$.question1yes.title = '\u2713';
+				}
+				else
+				{
+					$.question1no.value = true;
+					$.question1no.backgroundColor = '#007690';
+					$.question1no.title = '\u2713';
+				}
+				
+				if (json[i].FUNDING == 1)
+				{
+					$.question2yes.value = true;
+					$.question2yes.backgroundColor = '#007690';
+					$.question2yes.title = '\u2713';
+				}
+				else
+				{
+					$.question2no.value = true;
+					$.question2no.backgroundColor = '#007690';
+					$.question2no.title = '\u2713';
+				}
+			}	
+		}
+	};
+	
+	alert('end of readData function!');
+}
+
+function insertData(){ 
+	var request = Ti.Network.createHTTPClient({ 
+
+		//onload:alert("Your chore has been submitted"), 
+	
+		onerror: function(e){ 
+			Ti.API.debug(e.error); 
+			alert('There was an error during the connection'); 
+		}, 
+		timeout:1000, 	         
+	});  
+
+	//Request the data from the web service, Here you have to change it for your local ip 
+
+    request.open("POST","52.32.54.34/php/insert_into_user.php"); 
+       
+	var params = ({ "USER_ID": 				'0123456789',
+					"NAME": 				$.nameField.value, 
+					"PHONE": 				'TEST1', 
+					"EDUCATION": 			$.educationText.value, 
+					"CURRENT_PROJ": 	    $.projectText.value, 
+					"AREA_EXPERTISE":       $.expertiseText.value, 
+					"COMMITTEES" : 			$.committeeText.value, 
+					"OTHER_INTERESTS": 		$.otherInterestText.value,  
+					"O_CONTACT_INFO": 		$.contactInfoText.value,
+					"FUNDING": 				getCheckboxValue($.question2yes),
+					"EXPAND": 				getCheckboxValue($.question1yes),
+					});
+      
+	request.send(params);
+	alert('Successfully saved!');
+};
+
+
 $.profile.open();
+//readData();
