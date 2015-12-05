cf525da49ce705c59f477f054c00252c24e23e28
necatiozturk <nozturk@islander.tamucc.edu>
2015-12-04 20:18:28 -0600

    added comments to credits

--- facultyApp/app/controllers/credits.js
+++ facultyApp/app/controllers/credits.js
@@ -1,9 +1,18 @@
+/**
+ * Instantiate the local variables for this controller
+ */
 var args = arguments[0] || {};
 
+/**
+ *	Home button function will take you to Main Menu
+ */
 var homeButtonFunc = function () {
 	Alloy.Globals.goToHome ($, $.credits);
 };
 
+/**
+ *	Use the androidback event to go back to Main Menu
+ */
 $.credits.addEventListener('androidback' , function (e) {
 	Alloy.Globals.goToHome ($, $.credits);
 });
